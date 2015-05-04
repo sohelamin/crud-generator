1c9ef99fdcedd5cfab6a4c9e4755415503cc672f
sohelamin <sohelamincse@gmail.com>
2015-05-04 20:22:01 +0600

    Initial Commit

---
+++ src/CrudGeneratorServiceProvider.php
@@ -0,0 +1,40 @@
+<?php namespace Appzcoder\CrudGenerator;
+
+use Illuminate\Support\ServiceProvider;
+
+class CrudGeneratorServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+        $this->commands(
+        	'Appzcoder\CrudGenerator\Commands\CrudCommand',
+			'Appzcoder\CrudGenerator\Commands\CrudControllerCommand',
+			'Appzcoder\CrudGenerator\Commands\CrudModelCommand',
+			'Appzcoder\CrudGenerator\Commands\CrudMigrationCommand',
+			'Appzcoder\CrudGenerator\Commands\CrudViewCommand'
+        );
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return [];
+	}
+
+}
