c96e4309635b001b50e2e9f31258cdaf017528e1
appzcoder <appzcoder@gmail.com>
2015-06-08 18:08:01 +0600

    UI Updated
    UI Updated

--- src/CrudViewCommand.php
+++ src/CrudViewCommand.php
@@ -161,9 +161,11 @@ class CrudViewCommand extends Command {
         } 
 
         $layoutsFile = __DIR__.'/stubs/master.blade.stub';
-        $newLayoutsFile = $path.'master.blade.php';
+        $newLayoutsFile = $layoutsDirPath.'master.blade.php';
         if (!copy($layoutsFile, $newLayoutsFile)) {
             echo "failed to copy $layoutsFile...\n";
+        } else {
+            file_get_contents($newLayoutsFile);
         }
 
         $this->info('View created successfully.');        
