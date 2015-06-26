f71f58ac7fb9b4519ca63614d65a2dc3e0af343e
Sohel Amin <sohelamincse@gmail.com>
2015-06-26 20:59:59 +0600

    PSR-2 Refactored
    PSR-2 Refactored

--- src/CrudGeneratorServiceProvider.php
+++ src/CrudGeneratorServiceProvider.php
@@ -1,40 +1,43 @@
-<?php namespace Appzcoder\CrudGenerator;
+<?php
+
+namespace Appzcoder\CrudGenerator;
 
 use Illuminate\Support\ServiceProvider;
 
-class CrudGeneratorServiceProvider extends ServiceProvider {
+class CrudGeneratorServiceProvider extends ServiceProvider
+{
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
         $this->commands(
-        	'Appzcoder\CrudGenerator\CrudCommand',
-			'Appzcoder\CrudGenerator\CrudControllerCommand',
-			'Appzcoder\CrudGenerator\CrudModelCommand',
-			'Appzcoder\CrudGenerator\CrudMigrationCommand',
-			'Appzcoder\CrudGenerator\CrudViewCommand'
+            'Appzcoder\CrudGenerator\CrudCommand',
+            'Appzcoder\CrudGenerator\CrudControllerCommand',
+            'Appzcoder\CrudGenerator\CrudModelCommand',
+            'Appzcoder\CrudGenerator\CrudMigrationCommand',
+            'Appzcoder\CrudGenerator\CrudViewCommand'
         );
-	}
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return [];
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
 
 }
