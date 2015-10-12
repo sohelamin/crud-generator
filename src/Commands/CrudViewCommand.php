91493bbedf82cf2f6255f33d4f3ddad001615dda
Andrea Bergamasco <abergamasco@gmail.com>
2015-10-12 23:25:59 +0200

    Fixed slash bug in view generator

--- src/Commands/CrudViewCommand.php
+++ src/Commands/CrudViewCommand.php
@@ -133,7 +133,7 @@ class CrudViewCommand extends Command
             $formHeadingHtml .= '<th>' . $label . '</th>';
 
             if ($i == 0) {
-                $formBodyHtml .= '<td><a href="{{ url(\'%%urlPrefix%%/%%crudName%%\', $item->id) }}">{{ $item->' . $field . ' }}</a></td>';
+                $formBodyHtml .= '<td><a href="{{ url(\'%%urlPrefix%%%%crudName%%\', $item->id) }}">{{ $item->' . $field . ' }}</a></td>';
             } else {
                 $formBodyHtml .= '<td>{{ $item->' . $field . ' }}</td>';
             }
