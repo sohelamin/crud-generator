6138d566601fefc8f13fd10d7279c13a80e0ce58
Jean-Guy Landriault <landjea@users.noreply.github.com>
2016-03-29 10:41:36 -0400

    Update CrudConfigCommand.php

--- src/Commands/CrudConfigCommand.php
+++ src/Commands/CrudConfigCommand.php
@@ -23,23 +23,45 @@ class CrudConfigCommand extends Command
     protected $description = 'Create a new config.';
 
     /**
+     * Name of the Crud.
+     *
+     * @var string
+     */
+    protected $crudName = '';
+
+    /**
+     * Crud Name in capital form.
+     *
+     * @var string
+     */
+    protected $crudNameCap = '';
+
+    /**
+     * Stub file
+     *
+     * @var string
+     */
+    protected $stub = '';
+    
+
+    /**
      * Execute the console command.
      *
      * @return mixed
      */
     public function handle()
     {
-        $crudName = strtolower( $this->argument('name') );
-        $crudNameCap = ucwords($crudName);
+        $this->crudName = strtolower( $this->argument('name') );
+        $this->crudNameCap = ucwords( $this->crudName );
 
-        $stub = config('crudgenerator.custom_template') ? config('crudgenerator.path') . 'config.stub' : __DIR__ . '/../stubs/config.stub';
+        $this->stub = config('crudgenerator.custom_template') ? config('crudgenerator.path') . 'config.stub' : __DIR__ . '/../stubs/config.stub';
 
-        $newConfigFile = config_path() . '/'. $crudName .'.php';
-        if ( ! File::copy($stub, $newConfigFile)) {
+        $newConfigFile = config_path() . '/'. $this->crudName .'.php';
+        if ( ! File::copy($this->stub, $newConfigFile)) {
             $this->info("failed to copy $stub...\n");
         } else {
-            File::put($newConfigFile, str_replace('%%crudNameLower%%', $crudName, File::get($newConfigFile)));
-            File::put($newConfigFile, str_replace('%%crudNameUpper%%', $crudNameCap, File::get($newConfigFile)));
+            File::put($newConfigFile, str_replace('%%crudNameLower%%', $this->crudName, File::get($newConfigFile)));
+            File::put($newConfigFile, str_replace('%%crudNameUpper%%', $this->crudNameCap, File::get($newConfigFile)));
 
             $this->info('Config created successfully.');
         }
