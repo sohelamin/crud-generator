ab6805c276ab79d139dda24dcf58802b50fd00ae
Sohel Amin <sohelamincse@gmail.com>
2016-09-29 00:05:53 +0600

    Removed a file

--- src/stubs/form.blade.php
+++
@@ -1,7 +0,0 @@
-%%formFieldsHtml%%
-
-<div class="form-group">
-    <div class="col-md-offset-4 col-md-4">
-        {!! Form::submit(isset($submitButtonText) ? $submitButtonText : 'Create', ['class' => 'btn btn-primary']) !!}
-    </div>
-</div>
