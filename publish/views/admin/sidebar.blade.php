6fb79c76e15896495935959e2791379df6c8d0a8
Sohel Amin <sohelamincse@gmail.com>
2017-02-11 15:44:32 +0600

    FIx urls

--- publish/views/admin/sidebar.blade.php
+++ publish/views/admin/sidebar.blade.php
@@ -1,14 +1,14 @@
 <div class="col-md-3">
     <div class="panel panel-default panel-flush">
         <div class="panel-heading">
-            Section
+            Sidebar
         </div>
 
         <div class="panel-body">
             <ul class="nav" role="tablist">
                 <li role="presentation">
-                    <a href="#">
-                        #
+                    <a href="{{ url('/admin') }}">
+                        Dashboard
                     </a>
                 </li>
             </ul>
