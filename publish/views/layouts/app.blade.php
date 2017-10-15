5da53e7d9358f7aa10ae6f27d184d5d5c504c80a
Sohel Amin <sohelamincse@gmail.com>
2017-10-16 02:03:39 +0600

    Refactore some stub files

--- publish/views/layouts/app.blade.php
+++ publish/views/layouts/app.blade.php
@@ -79,6 +79,15 @@
             </div>
         </nav>
 
+        @if (session()->has('flash_message'))
+            <div class="container">
+                <div class="alert alert-success">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                    {{ session()->get('flash_message') }}
+                </div>
+            </div>
+        @endif
+
         @yield('content')
     </div>
 
