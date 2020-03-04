c4913e06387e4106ddee6f3323d6186aec0f1fc0
Sohel Amin <sohelamincse@gmail.com>
2020-03-05 01:26:24 +0600

    Update service provider

--- src/CrudGeneratorServiceProvider.php
+++ src/CrudGeneratorServiceProvider.php
@@ -28,12 +28,6 @@ class CrudGeneratorServiceProvider extends ServiceProvider
             __DIR__ . '/../publish/views/' => base_path('resources/views/'),
         ]);
 
-        if (\App::VERSION() <= '5.2') {
-            $this->publishes([
-                __DIR__ . '/../publish/css/app.css' => public_path('css/app.css'),
-            ]);
-        }
-
         $this->publishes([
             __DIR__ . '/stubs/' => base_path('resources/crud-generator/'),
         ]);
